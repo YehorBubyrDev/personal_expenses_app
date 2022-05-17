74fe3437686e5431f0f00e916eaf05c3ed384749
YehorBubyrDev <yehor.bubyr@uplight.com>
2022-05-17 11:00:34 +0300

    update chart bar money title

--- lib/widgets/Chart/chart_bar.dart
+++ lib/widgets/Chart/chart_bar.dart
@@ -19,7 +19,7 @@ class ChartBar extends StatelessWidget {
         Container(
           height: 20,
           child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+            child: Text('\₴${spendingAmount.toStringAsFixed(0)}'),
           ),
         ),
         const SizedBox(height: 4),
